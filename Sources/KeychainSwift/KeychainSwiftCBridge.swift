915bd9240fd8f8871ffe949def8d7ab8cc5fd97a
Scott Little <scott@littleknownsoftware.com>
2025-06-18 18:21:55 +0200

    Fixes warning for sift 6.2!

--- Sources/KeychainSwift/KeychainSwiftCBridge.swift
+++ Sources/KeychainSwift/KeychainSwiftCBridge.swift
@@ -26,7 +26,7 @@ import KeychainBase
  3. You might need to remove `import KeychainSwift` import from this file in your project.
  
 */
-@objc (KeychainSwift)
+@objc(KeychainSwift)
 public class KeychainSwiftCBridge: NSObject {
 	let keychain = KeychainSwift()
 		
@@ -66,118 +66,118 @@ public class KeychainSwiftCBridge: NSObject {
 	}
 
 	
-	@objc (setPassword:forKey:)
+	@objc(setPassword:forKey:)
 	@discardableResult
 	open func set(_ value: String, forKey key: String) -> Bool {
 		return keychain.set(value, forKey: key)
 	}
 	
-	@objc (setPassword:forKey:service:)
+	@objc(setPassword:forKey:service:)
 	@discardableResult
 	open func set(_ value: String, forKey key: String, service: String) -> Bool {
 		return keychain.set(value, forKey: key, service: service)
 	}
 	
-	@objc (setPassword:forKey:service:label:)
+	@objc(setPassword:forKey:service:label:)
 	@discardableResult
 	open func set(_ value: String, forKey key: String, service: String, label: String) -> Bool {
 		return keychain.set(value, forKey: key, service: service, label: label)
 	}
 	
-	@objc (setData:forKey:)
+	@objc(setData:forKey:)
 	@discardableResult
 	open func setData(_ value: Data, forKey key: String) -> Bool {
 		return keychain.set(value, forKey: key)
 	}
 	
-	@objc (setData:forKey:service:)
+	@objc(setData:forKey:service:)
 	@discardableResult
 	open func setData(_ value: Data, forKey key: String, service: String) -> Bool {
 		return keychain.set(value, forKey: key, service: service)
 	}
 	
-	@objc (setData:forKey:service:label:)
+	@objc(setData:forKey:service:label:)
 	@discardableResult
 	open func setData(_ value: Data, forKey key: String, service: String, label: String) -> Bool {
 		return keychain.set(value, forKey: key, service: service, label: label)
 	}
 	
-	@objc (setBool:forKey:)
+	@objc(setBool:forKey:)
 	@discardableResult
 	open func setBool(_ value: Bool, forKey key: String) -> Bool {
 		return keychain.set(value, forKey: key)
 	}
 	
-	@objc (setBool:forKey:service:)
+	@objc(setBool:forKey:service:)
 	@discardableResult
 	open func setBool(_ value: Bool, forKey key: String, service: String) -> Bool {
 		return keychain.set(value, forKey: key, service: service)
 	}
 	
-	@objc (setBool:forKey:service:label:)
+	@objc(setBool:forKey:service:label:)
 	@discardableResult
 	open func setBool(_ value: Bool, forKey key: String, service: String, label: String) -> Bool {
 		return keychain.set(value, forKey: key, service: service, label: label)
 	}
 	
-	@objc (passwordForKey:)
+	@objc(passwordForKey:)
 	open func get(_ key: String) -> String? {
 		return keychain.get(key)
 	}
 	
-	@objc (passwordForKey:service:)
+	@objc(passwordForKey:service:)
 	open func get(_ key: String, service: String) -> String? {
 		return keychain.get(key, service: service)
 	}
 	
-	@objc (passwordForKey:service:label:)
+	@objc(passwordForKey:service:label:)
 	open func get(_ key: String, service: String, label: String) -> String? {
 		return keychain.get(key, service: service, label: label)
 	}
 	
-	@objc (dataForKey:)
+	@objc(dataForKey:)
 	open func getData(_ key: String) -> Data? {
 		return keychain.getData(key)
 	}
 	
-	@objc (dataForKey:service:)
+	@objc(dataForKey:service:)
 	open func getData(_ key: String, service: String) -> Data? {
 		return keychain.getData(key, service: service)
 	}
 	
-	@objc (dataForKey:service:label:)
+	@objc(dataForKey:service:label:)
 	open func getData(_ key: String, service: String, label: String) -> Data? {
 		return keychain.getData(key, service: service, label: label)
 	}
 
-	@objc (boolForKey:)
+	@objc(boolForKey:)
 	open func getBool(_ key: String) -> Bool {
 		return keychain.getBool(key) ?? false
 	}
 	
-	@objc (boolForKey:service:)
+	@objc(boolForKey:service:)
 	open func getBool(_ key: String, service: String) -> Bool {
 		return keychain.getBool(key, service: service) ?? false
 	}
 	
-	@objc (boolForKey:service:label:)
+	@objc(boolForKey:service:label:)
 	open func getBool(_ key: String, service: String, label: String) -> Bool {
 		return keychain.getBool(key, service: service, label: label) ?? false
 	}
 	
-	@objc (deleteForKey:)
+	@objc(deleteForKey:)
 	@discardableResult
 	open func delete(_ key: String) -> Bool {
 		return keychain.delete(key);
 	}
 	
-	@objc (deleteForKey:service:)
+	@objc(deleteForKey:service:)
 	@discardableResult
 	open func delete(_ key: String, service: String) -> Bool {
 		return keychain.delete(key, service: service);
 	}
 	
-	@objc (deleteForKey:service:label:)
+	@objc(deleteForKey:service:label:)
 	@discardableResult
 	open func delete(_ key: String, service: String, label: String) -> Bool {
 		return keychain.delete(key, service: service, label: label);
@@ -191,25 +191,25 @@ public class KeychainSwiftCBridge: NSObject {
 	
 	//	MARK: Legacy Management
 
-	@objc (migratePasswordForKey:service:)
+	@objc(migratePasswordForKey:service:)
 	@discardableResult
 	open func migratePassword(_ key: String, service: String) -> String? {
 		return keychain.migratePassword(key, service: service)
 	}
 	
-	@objc (migratePasswordForKey:service:label:)
+	@objc(migratePasswordForKey:service:label:)
 	@discardableResult
 	open func migratePassword(_ key: String, service: String, label: String) -> String? {
 		return keychain.migratePassword(key, service: service, label: label)
 	}
 
-	@objc (migrateDataForKey:service:)
+	@objc(migrateDataForKey:service:)
 	@discardableResult
 	open func migrateData(_ key: String, service: String) -> Data? {
 		return keychain.migrateData(key, service: service)
 	}
 	
-	@objc (migrateDataForKey:service:label:)
+	@objc(migrateDataForKey:service:label:)
 	@discardableResult
 	open func migrateData(_ key: String, service: String, label: String) -> Data? {
 		return keychain.migrateData(key, service: service, label: label)
@@ -218,41 +218,41 @@ public class KeychainSwiftCBridge: NSObject {
 
 	//	MARK: Account extension
 	
-	@objc (passwordForAccount:)
+	@objc(passwordForAccount:)
 	open func password(account: KeychainAccount) -> String? {
 		return keychain.password(account: account)
 	}
 	
-	@objc (dataForAccount:)
+	@objc(dataForAccount:)
 	open func data(account: KeychainAccount) -> Data? {
 		return keychain.data(account: account)
 	}
 	
-	@objc (deletePasswordForAccount:)
+	@objc(deletePasswordForAccount:)
 	@discardableResult
 	open func delete(account: KeychainAccount) -> Bool {
 		return keychain.deletePassword(account: account)
 	}
 	
-	@objc (deleteInfoForAccount:)
+	@objc(deleteInfoForAccount:)
 	@discardableResult
 	open func deleteInfo(account: KeychainAccount) -> Bool {
 		return keychain.deleteStore(account: account)
 	}
 	
-	@objc (setPassword:forAccount:)
+	@objc(setPassword:forAccount:)
 	@discardableResult
 	open func set(_ value: String, forAccount account: KeychainAccount) -> Bool {
 		return keychain.set(value, account: account)
 	}
 	
-	@objc (setData:forAccount:)
+	@objc(setData:forAccount:)
 	@discardableResult
 	open func setData(_ value: Data, forAccount account: KeychainAccount) -> Bool {
 		return keychain.set(value, account: account)
 	}
 	
-	@objc (retrieveInfoForAccount:)
+	@objc(retrieveInfoForAccount:)
 	open func retrieve(_ account: KeychainAccount) -> [String: Any]? {
 		return keychain.retrieve(account: account)
 	}
